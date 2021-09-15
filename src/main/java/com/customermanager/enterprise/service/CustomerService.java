76f1cbd08131693071769d7f39bce1a4176da2c0
RIA03B <raniaibrahim1234567890@gmail.com>
2021-09-15 06:49:44 -0400

    Implement an Interface.

--- src/main/java/com/customermanager/enterprise/service/CustomerService.java
+++ src/main/java/com/customermanager/enterprise/service/CustomerService.java
@@ -2,7 +2,6 @@ package com.customermanager.enterprise.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import com.customermanager.enterprise.dao.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
