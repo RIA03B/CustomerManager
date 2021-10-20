46fb36bf9cc6810e9255c67fec00f62e91741511
cturn <cturner0098@gmail.com>
2021-10-20 18:53:42 -0400

    refactored customer -> CustomerDTO

--- src/main/java/com/customermanager/enterprise/controller/CustomerController.java
+++ src/main/java/com/customermanager/enterprise/controller/CustomerController.java
@@ -1,6 +1,6 @@
 package com.customermanager.enterprise.controller;
 
-import com.customermanager.enterprise.dto.Customer;
+import com.customermanager.enterprise.dto.CustomerDTO;
 import com.customermanager.enterprise.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,35 +16,35 @@ public class CustomerController {
     private CustomerService customerService;
     // display list of Customers
     @GetMapping("/")
-    public String viewHomePage(Model model) {
+    public String viewHomePage(Model model) throws Exception {
         model.addAttribute("listCustomer", customerService.getAllCustomers());
         return "index";
     }
     @GetMapping("/showNewCustomerForm")
     public String showNewCustomerForm(Model model) {
         // create model attribute to bind form data
-        Customer customer = new Customer();
+        CustomerDTO customer = new CustomerDTO();
         model.addAttribute("customer", customer);
         return "new_customer";
     }
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("customer") Customer customer) {
+    public String saveEmployee(@ModelAttribute("customer") CustomerDTO customer) throws Exception {
         // save employee to database
         customerService.save(customer);
         return "redirect:/";
     }
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable(value = "id") long id, Model model) {
+    public String edit(@PathVariable(value = "id") int id, Model model) {
 
         // get customer from the service
-        Customer customer = customerService.get(id);
+        CustomerDTO customer = customerService.getCustomerById(id);
 
         // set customer as a model attribute to pre-populate the form
         model.addAttribute("customer", customer);
         return "edit_customer";
     }
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable(value = "id") long id) {
+    public String delete(@PathVariable(value = "id") int id) {
 
         // call delete customer method
         this.customerService.delete(id);
@@ -52,7 +52,7 @@ public class CustomerController {
     }
     @GetMapping("/search")
     public ModelAndView search(@RequestParam String keyword) {
-        List<Customer> result = customerService.search(keyword);
+        List<CustomerDTO> result = customerService.search(keyword);
         ModelAndView mav = new ModelAndView("search");
         mav.addObject("result", result);
 
